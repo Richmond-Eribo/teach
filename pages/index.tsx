cf81778d2b3ca0bf24853729ec3d812383b3ef8c
Richmond Eribo <uyiosaeribo344@gmail.com>
2022-06-03 00:41:40 +0100

    Increased margin for the site

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,12 @@ import TextWrapper from '../components/TextWrapper'
 const Home: NextPage = () => {
   return (
     <div className=''>
-      <section className=' px-4 pt-8 bg-[#D1644D]/5 lg:px-10 lg:pt-10'>
+      <section className=' px-6 pt-8 bg-[#D1644D]/5 lg:px-10 lg:pt-10 '>
         <Nav />
         <LandingPageSlide />
       </section>
 
-      <section className='px-4 py-10 lg:px-36 lg:py-20' id='About us'>
+      <section className='px-6 py-10 lg:px-36 lg:py-20' id='About us'>
         <h2 className='heading-1'>About Us</h2>
 
         <div className='grid grid-cols-1  grid-rows-1  lg:grid-cols-2 lg:gap-28 justify-around items-center '>
@@ -101,7 +101,7 @@ const Home: NextPage = () => {
       </section>
 
       <section
-        className='lg:px-36 px-4 py-10 lg:py-20 bg-[#42B9D1]/5'
+        className='lg:px-36 px-6 py-10 lg:py-20 bg-[#42B9D1]/5'
         id='Services'
       >
         <h2 className='heading-1'>Services</h2>
@@ -200,7 +200,7 @@ const Home: NextPage = () => {
       <section className='lg:px-36 px-4 py-10'>
         <h2 className='heading-1'>University Admission Assist</h2>
 
-        <div className='flex flex-col lg:flex-row text-text-light justify-around'>
+        <div className='flex px-2 flex-col lg:flex-row text-text-light justify-around'>
           <article className='lg:w-[400px]'>
             <p className='mb-5'>
               We are UK based outstanding teachers providing 1st class tuition
@@ -246,7 +246,7 @@ const Home: NextPage = () => {
         </div>
       </section>
 
-      <section className=' bg-[#42B9D1]/5 flex lg:flex-row lg:justify-around flex-col items-start px-4 lg:px-36 py-10'>
+      <section className=' bg-[#42B9D1]/5 flex lg:flex-row lg:justify-around flex-col items-start px-6 lg:px-36 py-10'>
         <div className='text-text-light w-[248px] mb-10'>
           <h2 className='font-bold lg:text-4xl text-[35px]  text-text-dark lg:mb-10'>
             Reach Us
